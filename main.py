1dfa420ed6a1ff1ee9c47610acfbf06695662a71
Alb111 <albertfelix2005@gmail.com>
2023-10-10 18:12:50 -0700

    setting up countdown in CustomTkinter

--- main.py
+++ main.py
@@ -1,64 +1,70 @@
 import time
 import datetime
-class Pondomoro:
+import tkinter
+import customtkinter
+
+class Pomodoro:
     
-    def __init__(self):
-        self.studyTime=0
-        self.breakTime=0
-        self.studyTrue=True
-        self.repeats=0
+    def __init__(self, root):
+        self.studyTime = 0
+        self.breakTime = 0
+        self.studyTrue = True
+        self.repeats = 0
+        self.root = root
+        self.label = customtkinter.CTkLabel(root, text="")
+        self.label.pack()
     
-    def setStudyTime(self,x):
-        self.studyTime=x
+    def setStudyTime(self, x):
+        self.studyTime = x
     
     def getStudyTime(self):
         return self.studyTime
     
-    def setBreakTime(self,x):
-        self.breakTime=x
+    def setBreakTime(self, x):
+        self.breakTime = x
     
     def getBreakTime(self):
         return self.breakTime
     
-    def setStudyTrue(self,x):
-        self.studyTrue=x
+    def setStudyTrue(self, x):
+        self.studyTrue = x
     
     def getStudyTrue(self):
         return self.studyTrue
     
-    def setRepeats(self,x):
-        self.repeats=x
+    def setRepeats(self, x):
+        self.repeats = x
     
     def getRepeats(self):
         return self.repeats
     
     def stopwatch(self, total_seconds):
-        
-        while(total_seconds!=0):
-            timer=datetime.timedelta(seconds = total_seconds)
-            print(timer, end="\r")
+        while total_seconds > 0:
+            timer = datetime.timedelta(seconds=total_seconds)
+            self.label.configure(text=timer)
+            self.root.update()
             time.sleep(1)
             total_seconds -= 1
     
     def start(self):
-        while(self.repeats>0):
-            if(self.studyTrue):
-                self.stopwatch(self.studyTime * 60 )
-                self.studyTrue=False
+        while self.repeats > 0:
+            if self.studyTrue:
+                self.stopwatch(self.studyTime * 60)
+                self.studyTrue = False
             else:
-                self.stopwatch(total_seconds = self.breakTime * 60)
-                self.studyTrue=False
-            self.repeats=self.repeats-1
+                self.stopwatch(self.breakTime * 60)
+                self.studyTrue = True
+            self.repeats -= 1
 
-x=Pondomoro()
-x.setBreakTime(.5)
-x.setStudyTime(.25)
+root = customtkinter.CTk()
+root.title('Pomodoro Timer')
+root.geometry("500x100")
+x = Pomodoro(root)
+x.setStudyTime(10)
+x.setBreakTime(5)
 x.setRepeats(2)
 x.start()
-
-        
-        
-
+root.mainloop()
 
 
         
